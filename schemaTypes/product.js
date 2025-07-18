d0b40984c2b73ee06b16649e25dcc38bdd21c61d
holyherro <khususgame475@gmail.com>
2025-07-18 14:00:26 +0700

    Add files via upload

---
+++ schemaTypes/product.js
@@ -0,0 +1,78 @@
+import {defineField, defineType} from 'sanity'
+
+export default defineType({
+  name: 'product',
+  title: 'Product',
+  type: 'document',
+  fields: [
+    defineField({
+      name: 'title',
+      title: 'Title',
+      type: 'string',
+    }),
+    defineField({
+      name: 'price',
+      title: 'Price',
+      type: 'string'
+    }),
+    defineField({
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      options: {
+        source: 'title',
+        maxLength: 96,
+      },
+    }),
+    defineField({
+      name: 'thumbnail',
+      title: 'Thumbnail',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+    }),
+    defineField({
+      name: 'categories',
+      title: 'Categories',
+      type: 'array',
+      of: [{type: 'reference', to: {type: 'category'}}],
+    }),
+    defineField({
+      name: 'publishedAt',
+      title: 'Published at',
+      type: 'datetime',
+    }),
+    defineField({
+      name: 'images',
+      type: 'array',
+      title: 'Images',
+      of: [
+        { type: 'image' }
+      ],
+      options: {
+        layout: 'list'
+      },
+    }),
+    defineField({
+      name: 'description',
+      title: 'Description',
+      type: 'blockContent',
+    }),
+    defineField({
+      name: 'latitude',
+      title: 'Latitude',
+      type: 'string'
+    }),
+    defineField({
+      name: 'longitude',
+      title: 'Longitude',
+      type: 'string'
+    }),
+    defineField({
+      name: 'location',
+      title: 'Location',
+      type: 'string',
+    })
+  ],
+})
